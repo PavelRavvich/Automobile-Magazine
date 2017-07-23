322ea447665b70f1543184ca9fbde8fe55974990
Pavel <triodjangopiter@yandex.ru>
2017-07-23 21:49:50 +0300

    add get user by login & password and add get propose with user

--- src/main/java/ru/pravvich/dao/DAO.java
+++ src/main/java/ru/pravvich/dao/DAO.java
@@ -1,4 +1,24 @@
 package ru.pravvich.dao;
 
-public interface UserDAO {
+
+import ru.pravvich.model.Propose;
+import ru.pravvich.model.User;
+
+public interface DAO {
+    /**
+     * Get user by login & password.
+     *
+     * @param login of Propose.
+     * @param password of Propose.
+     * @return Propose with appropriate pair login & password.
+     */
+    User getUser(String login, String password);
+
+    /**
+     * Get propose by Propose id.
+     *
+     * @param id of Propose.
+     * @return Propose with appropriate id.
+     */
+    Propose getProposeId(int id);
 }
--- src/main/java/ru/pravvich/dao/DAOImp.java
+++ src/main/java/ru/pravvich/dao/DAOImp.java
@@ -1,12 +1,65 @@
 package ru.pravvich.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import ru.pravvich.model.Propose;
+import ru.pravvich.model.User;
+
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 23.07.17.
  * <p>
- * UserDAOImp
+ * DAOImp
  */
-public class UserDAOImp {
+public class DAOImp implements DAO {
+    /**
+     * Connection factory to database.
+     */
+    private final AtomicReference<SessionFactory> factory;
 
+    /**
+     * Default constructor.
+     *
+     * @param factory of connection to database.
+     */
+    public DAOImp(final AtomicReference<SessionFactory> factory) {
+        this.factory = factory;
+    }
 
+    @Override
+    public User getUser(String login, String password) {
+
+        User result;
+
+        final String hql = "select u from User u where u.login = :login and u.password = :password";
+
+        try (final Session session = factory.get().openSession()) {
+
+            result = session.createQuery(hql, User.class)
+                    .setParameter("login", login)
+                    .setParameter("password", password)
+                    .getSingleResult();
+        }
+
+        return result;
+    }
+
+    @Override
+    public Propose getProposeId(int id) {
+
+        Propose result;
+
+        try (final Session session = factory.get().openSession()) {
+
+            session.beginTransaction();
+
+            result = session.get(Propose.class, 1);
+        }
+
+        return result;
+    }
 }

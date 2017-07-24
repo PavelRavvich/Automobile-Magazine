c7ff2b990d9c9191af7561be2a447951a2ca0941
Pavel <triodjangopiter@yandex.ru>
2017-07-24 17:34:29 +0300

    add WebListener for inject DAO layer before load servlets

--- src/main/java/ru/pravvich/servlet/ContextListener.java
+++ src/main/java/ru/pravvich/servlet/ContextListener.java
@@ -1,12 +1,37 @@
 package ru.pravvich.servlet;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.pravvich.dao.DAOImp;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 24.07.17.
  * <p>
  * ContextListener
  */
-public class ContextListener {
+@WebListener
+public class ContextListener implements ServletContextListener {
 
+    private final SessionFactory factory = new Configuration()
+            .configure()
+            .buildSessionFactory();
 
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        final DAOImp dao = new DAOImp(new AtomicReference<>(factory));
+
+        sce.getServletContext().setAttribute("dao", dao);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        factory.close();
+    }
 }

2ded8567a82201f2a65fcb9af681481aba78346a
Pavel <triodjangopiter@yandex.ru>
2017-07-26 15:04:18 +0300

    add Servlet for get list of all proposes

--- src/main/java/ru/pravvich/servlet/AllProposesServlet.java
+++ src/main/java/ru/pravvich/servlet/AllProposesServlet.java
@@ -1,24 +1,33 @@
 package ru.pravvich.servlet;
 
+import ru.pravvich.dao.DAO;
+import ru.pravvich.model.Propose;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class AllProposesServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        System.out.println("post");
         doGet(req, resp);
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        System.out.println("get");
+
+        final DAO dao = (DAO) getServletContext().getAttribute("dao");
+
+        final List<Propose> allProposes = dao.getAllProposes();
+
+        req.setAttribute("allProposes", allProposes);
+
         req.getRequestDispatcher("/WEB-INF/view/all_proposes.jsp")
                 .forward(req, resp);
     }

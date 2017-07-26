86ff682403a95a8837b7b285719a208b2575badf
Pavel <triodjangopiter@yandex.ru>
2017-07-26 15:02:31 +0300

    add servlet get single propose by id

--- src/main/java/ru/pravvich/servlet/GetterProposeById.java
+++
@@ -1,9 +0,0 @@
-public class GetterProposeById extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
-    }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
-    }
-}
---
+++ src/main/java/ru/pravvich/servlet/ProposeByIdServlet.java
@@ -0,0 +1,31 @@
+package ru.pravvich.servlet;
+
+import ru.pravvich.dao.DAO;
+import ru.pravvich.model.Propose;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ProposeByIdServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        final int pId = Integer.valueOf(req.getParameter("pId"));
+
+        final DAO dao = (DAO) getServletContext().getAttribute("dao");
+
+        final Propose propose = dao.getProposeId(pId);
+
+        req.setAttribute("propose", propose);
+
+        System.out.println(propose);
+
+        req.getRequestDispatcher("/WEB-INF/view/single_propose.jsp")
+                .forward(req, resp);
+
+    }
+}

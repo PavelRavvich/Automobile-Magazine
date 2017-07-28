56580c124fde095a773b508cca480904325fdb7f
My Mac <MyMac@MacBook-Pro-My.local>
2017-07-28 04:48:56 +0300

    factory uploaders

--- src/main/java/ru/pravvich/util/UploadFactory.java
+++ src/main/java/ru/pravvich/util/UploadFactory.java
@@ -1,4 +1,32 @@
 package ru.pravvich.util;
 
-public class UploaderFactory {
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import java.io.File;
+
+public class UploadFactory {
+
+    private static final String tmp = "javax.servlet.context.tempdir";
+
+    public ServletFileUpload get(final ServletConfig config) {
+
+        // Create a factory for disk-based file items
+        final DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        // Set factory constraints
+        factory.setSizeThreshold(2_000_000);
+
+        // Configure a repository (to ensure a secure temp location is used)
+        final ServletContext servletContext = config.getServletContext();
+
+        final File repository = (File) servletContext.getAttribute(tmp);
+
+        factory.setRepository(repository);
+
+        // Create a new file upload handler
+        return new ServletFileUpload(factory);
+    }
 }

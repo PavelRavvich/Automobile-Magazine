6dce7ed54532ad45ed278bd582d6ad89d1f3f3a4
My Mac <MyMac@MacBook-Pro-My.local>
2017-07-28 04:50:12 +0300

    add test logic FileUpload fraimwork

--- src/main/java/ru/pravvich/servlet/AddProposeServlet.java
+++ src/main/java/ru/pravvich/servlet/AddProposeServlet.java
@@ -2,10 +2,12 @@ package ru.pravvich.servlet;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import ru.pravvich.util.FileReceiver;
+import ru.pravvich.util.TextReceiver;
+import ru.pravvich.util.UploadFactory;
 
-import javax.servlet.ServletContext;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,62 +15,42 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class AddProposeServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        final String mark = req.getParameter("mark");
-        final String model = req.getParameter("model");
-        final String description = req.getParameter("description");
 
+        final ServletConfig config = this.getServletConfig();
 
-        boolean isMultipart = ServletFileUpload.isMultipartContent(req);
+        final ServletFileUpload upload = new UploadFactory().get(config);
 
-
-        // Create a factory for disk-based file items
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-
-        // Configure a repository (to ensure a secure temp location is used)
-        ServletContext servletContext = this.getServletConfig().getServletContext();
-        File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
-        factory.setRepository(repository);
-
-        // Create a new file upload handler
-        ServletFileUpload upload = new ServletFileUpload(factory);
-
-        List<Byte> image = new ArrayList<>();
+        // Set overall request size constraint
+        upload.setSizeMax(10_000_000);
 
         // Parse the request
         try {
             List<FileItem> items = upload.parseRequest(req);
-            for (FileItem item : items) {
-                final byte[] bytes = item.get();
-                for (byte b : bytes) {
-                    image.add(b);
-                }
-            }
-
 
             File file = new File("/Users/MyMac/Desktop/im.jpg");
-            final boolean success = file.createNewFile();
-            Byte[] res = new Byte[image.size()];
-            final Byte[] bytes = image.toArray(res);
-            byte[] resu = new byte[bytes.length];
-            for (int i = 0; i < bytes.length; i++) {
-                resu[i] = bytes[i];
-            }
-            Files.write(file.toPath(), resu);
+
+            final byte[] receive = new FileReceiver().receive(items);
+
+            final Map<String, String> text = new TextReceiver().receive(items);
+
+            for (String s : text.values()) System.out.println(s);
+
+            Files.write(file.toPath(), receive);
 
         } catch (FileUploadException e) {
             e.printStackTrace();
         }
-        System.out.println(isMultipart);
-        System.out.println("!!!!!!!!!!!!!!!!!!!");
     }
+
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)

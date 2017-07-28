f942683c5d0985453968a3f8784e1c6d377ae6e6
My Mac <MyMac@MacBook-Pro-My.local>
2017-07-28 04:47:43 +0300

    add text heandler

--- src/main/java/ru/pravvich/util/TextReceiver.java
+++ src/main/java/ru/pravvich/util/TextReceiver.java
@@ -1,4 +1,30 @@
 package ru.pravvich.util;
 
+import org.apache.commons.fileupload.FileItem;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TextReceiver {
+
+    public Map<String, String> receive(final List<FileItem> items) {
+
+        final Map<String, String> result = new HashMap<>();
+
+        for (final FileItem item : items) {
+
+            if (item.isFormField()) {
+
+                final String name = item.getFieldName();
+
+                final String value = item.getString();
+
+
+                result.put(name, value);
+            }
+        }
+
+        return result;
+    }
 }

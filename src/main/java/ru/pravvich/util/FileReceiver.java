f543b087f3de19f8d9c808a53333118150c1e877
My Mac <MyMac@MacBook-Pro-My.local>
2017-07-28 04:46:31 +0300

    file controller

--- src/main/java/ru/pravvich/util/FileReceiver.java
+++ src/main/java/ru/pravvich/util/FileReceiver.java
@@ -1,4 +1,35 @@
 package ru.pravvich.util;
 
+import org.apache.commons.fileupload.FileItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileReceiver {
+
+    public byte[] receive(final List<FileItem> items) {
+
+        final List<Byte> entire = new ArrayList<>();
+
+        for (final FileItem item : items) {
+
+            if (!item.isFormField()) {
+
+                final byte[] bytes = item.get();
+
+                for (byte b : bytes) entire.add(b);
+            }
+        }
+
+        final Byte[] bytes = entire.toArray(new Byte[entire.size()]);
+
+        byte[] result = new byte[bytes.length];
+
+        for (int i = 0; i < result.length; i++) result[i] = bytes[i];
+
+        return result;
+    }
+
+
+
 }

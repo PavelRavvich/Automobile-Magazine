9656be7c4cc00fe12dfd297bd347cffd7bbe0261
Pavel <triodjangopiter@yandex.ru>
2017-08-02 03:07:53 +0300

    add servlet select logic

--- src/main/java/ru/pravvich/servlet/SelectProposeServlet.java
+++ src/main/java/ru/pravvich/servlet/SelectProposeServlet.java
@@ -1,12 +1,14 @@
 package ru.pravvich.servlet;
 
 import ru.pravvich.dao.DAO;
+import ru.pravvich.model.Propose;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class SelectProposeServlet extends HttpServlet {
 
@@ -16,7 +18,15 @@ public class SelectProposeServlet extends HttpServlet {
 
         final DAO dao = (DAO) req.getServletContext().getAttribute("dao");
 
+        final String mark = req.getParameter("mark");
 
+        final String model = req.getParameter("model");
 
+        final List<Propose> select = dao.select(mark, model);
+
+        req.setAttribute("allProposes", select);
+
+        req.getRequestDispatcher("/WEB-INF/view/all_proposes.jsp")
+                .forward(req, resp);
     }
 }
